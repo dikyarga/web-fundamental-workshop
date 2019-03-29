c4483d3528e89b0e2c53b38ae1c9d890eaa4db28
dikyarga <dikyarga.id@gmail.com>
2019-03-30 03:33:36 +0700

    render product to DOM

--- ecommerce.js
+++ ecommerce.js
@@ -4,7 +4,23 @@ function getProducts() {
     .then(result => {
     console.log('hasilnya : ', result)
 
-    document.querySelector('.products').innerHTML = JSON.stringify(result)
+    const resultHTML = result.map(product => {
+      return `
+          <div class="product">
+          <img
+            class="product-image"
+            src="${product.imageUrl}"
+            alt="${product.name}"
+          />
+          <h3>${product.name}</h3>
+          <h5 class="product-detail__price">Rp. ${product.price}</h5>
+          <p>${product.description}</p>
+          <button class="button is-primary">Beli Sekarang</button>
+        </div>
+      `
+    })
+
+    document.querySelector('.products').innerHTML = resultHTML
   })
 }
 
