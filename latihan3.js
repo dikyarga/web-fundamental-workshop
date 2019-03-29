5061925f5168d38d19f511c7b00a40aaa7e61452
dikyarga <dikyarga.id@gmail.com>
2019-03-30 03:13:20 +0700

    latihan JavaScript, variable, array, console and object

---
+++ latihan3.js
@@ -0,0 +1,30 @@
+var variableName = 'Isi variable'
+
+console.log('variable name :', variableName)
+
+let letName = 2343
+
+console.log('let name :', letName)
+
+const pi = 3.14
+
+console.log('pi :', pi)
+
+
+let person = {
+  name: 'Diky Arga'
+}
+
+console.log('objek person : ', person)
+
+const hobbies = ['make things', { name: 'abc'}, 17, 'repeat']
+
+console.log('array hobbies : ', hobbies)
+
+function calculateTotal(amount, price) {
+  return amount * price;
+}
+
+const total = calculateTotal(10, 1700)
+
+console.log('totalnya adalah ', total)
